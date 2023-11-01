0910cdbeee8f9d9503ded66c48e2ab0c871b8768
Base <143889306+Basepy@users.noreply.github.com>
2023-11-01 19:20:47 -0300

    Update configbot.py

--- configbot.py
+++ configbot.py
@@ -2,14 +2,6 @@
 #inserir token do bot entre as aspas
 bot_principal_token = "INSERIR_TOKEN"
 
-
-#Configurar parcelas
-parcelas_config = 3  
-#aqui não precisa de aspas. Use valores inteiros igual ao exemplo do lado
-
-#Taxa de serviço - Configure a taxa de serviço
-taxa_servico_config = 21.21
-
 # JUROS: Taxa % de quanto vc cobrará sob o valor requerido
 taxa_porcentagem = 100 #adicione o valor para o percentual que deseja cobrar
 
